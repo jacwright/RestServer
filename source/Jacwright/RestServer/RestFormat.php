834dbdd7f71adf6efaaec1e4fa08723e91c859f1
Tyler Dence <tyzoid.d@gmail.com>
2017-07-19 18:06:40 -0400

    #88 - Fix Braces and Spaces

--- source/Jacwright/RestServer/RestFormat.php
+++ source/Jacwright/RestServer/RestFormat.php
@@ -28,14 +28,13 @@ namespace Jacwright\RestServer;
 /**
  * Constants used in RestServer Class.
  */
-class RestFormat
-{
+class RestFormat {
 	const PLAIN = 'text/plain';
 	const HTML  = 'text/html';
 	const JSON  = 'application/json';
 	const XML   = 'application/xml';
 
-    /** @var array */
+	/** @var array */
 	static public $formats = array(
 		'plain' => RestFormat::PLAIN,
 		'txt'   => RestFormat::PLAIN,

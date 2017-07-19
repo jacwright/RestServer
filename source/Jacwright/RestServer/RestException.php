77fc1930a97ccd1bc08cf4f8298ac93904f48115
Tyler Dence <tyzoid.d@gmail.com>
2017-07-19 18:06:40 -0400

    #88 - Fix Braces and Spaces

--- source/Jacwright/RestServer/RestException.php
+++ source/Jacwright/RestServer/RestException.php
@@ -27,10 +27,8 @@ namespace Jacwright\RestServer;
 
 use Exception;
 
-class RestException extends Exception
-{
-    public function __construct($code, $message = null)
-    {
-        parent::__construct($message, $code);
-    }
+class RestException extends Exception {
+	public function __construct($code, $message = null) {
+		parent::__construct($message, $code);
+	}
 }
--- source/Jacwright/RestServer/RestServer.php
+++ source/Jacwright/RestServer/RestServer.php
@@ -39,8 +39,7 @@ use DOMDocument;
  *
  * @author jacob
  */
-class RestServer
-{
+class RestServer {
 	//@todo add type hint
 	public $url;
 	public $method;
@@ -62,12 +61,13 @@ class RestServer
 	 *
 	 * @param string $mode The mode, either debug or production
 	 */
-	public function  __construct($mode = 'debug', $realm = 'Rest Server')
-	{
+	public function  __construct($mode = 'debug', $realm = 'Rest Server') {
 		$this->mode = $mode;
 		$this->realm = $realm;
+
 		// Set the root
 		$dir = str_replace('\\', '/', dirname(str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME'])));
+
 		if ($dir == '.') {
 			$dir = '/';
 		} else {
@@ -75,11 +75,11 @@ class RestServer
 			if (substr($dir, -1) != '/') $dir .= '/';
 			if (substr($dir, 0, 1) != '/') $dir = '/' . $dir;
 		}
+
 		$this->root = $dir;
 	}
 
-	public function  __destruct()
-	{
+	public function  __destruct() {
 		if ($this->mode == 'production' && !$this->cached) {
 			if (function_exists('apc_store')) {
 				apc_store('urlMap', $this->map);
@@ -89,27 +89,24 @@ class RestServer
 		}
 	}
 
-	public function refreshCache()
-	{
+	public function refreshCache() {
 		$this->map = array();
 		$this->cached = false;
 	}
 
-	public function unauthorized($ask = false)
-	{
+	public function unauthorized($ask = false) {
 		if ($ask) {
 			header("WWW-Authenticate: Basic realm=\"$this->realm\"");
 		}
+
 		throw new RestException(401, "You are not authorized to access this resource.");
 	}
 
-	public function options()
-	{
+	public function options() {
 		throw new RestException(200, "authorized");
 	}
 
-	public function handle()
-	{
+	public function handle() {
 		$this->url = $this->getPath();
 		$this->method = $this->getMethod();
 		$this->format = $this->getFormat();
@@ -119,7 +116,7 @@ class RestServer
 		}
 
 		//preflight requests response 
-		if($this->method == 'OPTIONS' && getallheaders()->Access-Control-Request-Headers){
+		if ($this->method == 'OPTIONS' && getallheaders()->Access-Control-Request-Headers) {
 			$this->sendData($this->options());
 		}
 
@@ -149,34 +146,33 @@ class RestServer
 			} catch (RestException $e) {
 				$this->handleError($e->getCode(), $e->getMessage());
 			}
-
 		} else {
 			$this->handleError(404);
 		}
 	}
-	public function setRootPath($path)
-	{
+
+	public function setRootPath($path) {
 		$this->rootPath = '/'.trim($path, '/').'/';
 	}
-	public function setJsonAssoc($value)
-	{
+
+	public function setJsonAssoc($value) {
 		$this->jsonAssoc = ($value === true);
 	}
 
-	public function addClass($class, $basePath = '')
-	{
+	public function addClass($class, $basePath = '') {
 		$this->loadCache();
 
 		if (!$this->cached) {
-			if (is_string($class) && !class_exists($class)){
+			if (is_string($class) && !class_exists($class)) {
 				throw new Exception('Invalid method or class');
-			} elseif (!is_string($class) && !is_object($class)) {
+			} else if (!is_string($class) && !is_object($class)) {
 				throw new Exception('Invalid method or class; must be a classname or object');
 			}
 
 			if (substr($basePath, 0, 1) == '/') {
 				$basePath = substr($basePath, 1);
 			}
+
 			if ($basePath && substr($basePath, -1) != '/') {
 				$basePath .= '/';
 			}
@@ -185,25 +181,22 @@ class RestServer
 		}
 	}
 
-	public function addErrorClass($class)
-	{
+	public function addErrorClass($class) {
 		$this->errorClasses[] = $class;
 	}
 
-	public function handleError($statusCode, $errorMessage = null)
-	{
+	public function handleError($statusCode, $errorMessage = null) {
 		$method = "handle$statusCode";
+
 		foreach ($this->errorClasses as $class) {
 			if (is_object($class)) {
 				$reflection = new ReflectionObject($class);
-			} elseif (class_exists($class)) {
+			} else if (class_exists($class)) {
 				$reflection = new ReflectionClass($class);
 			}
 
-			if (isset($reflection))
-			{
-				if ($reflection->hasMethod($method))
-				{
+			if (isset($reflection)) {
+				if ($reflection->hasMethod($method)) {
 					$obj = is_string($class) ? new $class() : $class;
 					$obj->$method();
 					return;
@@ -211,8 +204,7 @@ class RestServer
 			}
 		}
 
-		if (!$errorMessage)
-		{
+		if (!$errorMessage) {
 			$errorMessage = $this->codes[$statusCode];
 		}
 
@@ -220,8 +212,7 @@ class RestServer
 		$this->sendData(array('error' => array('code' => $statusCode, 'message' => $errorMessage)));
 	}
 
-	protected function instantiateClass($obj) 
-	{
+	protected function instantiateClass($obj) {
 		if (class_exists($obj)) {
 			return new $obj();
 		}
@@ -229,15 +220,13 @@ class RestServer
 		return false;
 	}
 	
-	protected function initClass($obj) 
-	{
+	protected function initClass($obj) {
 		if (method_exists($obj, 'init')) {
 			$obj->init();
 		}
 	}
 	
-	protected function isAuthorizedByClass($obj) 
-	{
+	protected function isAuthorizedByClass($obj) {
 		if (method_exists($obj, 'authorize')) {
 			return $obj->authorize();
 		}
@@ -245,8 +234,7 @@ class RestServer
 		return true;
 	}
 	
-	protected function loadCache()
-	{
+	protected function loadCache() {
 		if ($this->cached !== null) {
 			return;
 		}
@@ -256,9 +244,10 @@ class RestServer
 		if ($this->mode == 'production') {
 			if (function_exists('apc_fetch')) {
 				$map = apc_fetch('urlMap');
-			} elseif (file_exists($this->cacheDir . '/urlMap.cache')) {
+			} else if (file_exists($this->cacheDir . '/urlMap.cache')) {
 				$map = unserialize(file_get_contents($this->cacheDir . '/urlMap.cache'));
 			}
+
 			if (isset($map) && is_array($map)) {
 				$this->map = $map;
 				$this->cached = true;
@@ -272,8 +261,7 @@ class RestServer
 		}
 	}
 
-	protected function findUrl()
-	{
+	protected function findUrl() {
 		$urls = $this->map[$this->method];
 		if (!$urls) return null;
 
@@ -289,27 +277,33 @@ class RestServer
 					} else {
 						$call[2] = array();
 					}
+
 					return $call;
 				}
 			} else {
 				$regex = preg_replace('/\\\\\$([\w\d]+)\.\.\./', '(?P<$1>.+)', str_replace('\.\.\.', '...', preg_quote($url)));
 				$regex = preg_replace('/\\\\\$([\w\d]+)/', '(?P<$1>[^\/]+)', $regex);
+
 				if (preg_match(":^$regex$:", urldecode($this->url), $matches)) {
 					$params = array();
 					$paramMap = array();
+
 					if (isset($args['data'])) {
 						$params[$args['data']] = $this->data;
 					}
 
 					foreach ($matches as $arg => $match) {
 						if (is_numeric($arg)) continue;
+
 						$paramMap[$arg] = $match;
 
 						if (isset($args[$arg])) {
 							$params[$args[$arg]] = $match;
 						}
 					}
+
 					ksort($params);
+
 					// make sure we have all the params we need
 					end($params);
 					$max = key($params);
@@ -318,20 +312,22 @@ class RestServer
 							$params[$i] = null;
 						}
 					}
+
 					ksort($params);
+
 					$call[2] = $params;
 					$call[3] = $paramMap;
+
 					return $call;
 				}
 			}
 		}
 	}
 
-	protected function generateMap($class, $basePath)
-	{
+	protected function generateMap($class, $basePath) {
 		if (is_object($class)) {
 			$reflection = new ReflectionObject($class);
-		} elseif (class_exists($class)) {
+		} else if (class_exists($class)) {
 			$reflection = new ReflectionClass($class);
 		}
 
@@ -341,7 +337,6 @@ class RestServer
 			$doc = $method->getDocComment();
 			$noAuth = strpos($doc, '@noAuth') !== false;
 			if (preg_match_all('/@url[ \t]+(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)[ \t]+\/?(\S*)/s', $doc, $matches, PREG_SET_ORDER)) {
-
 				$params = $method->getParameters();
 
 				foreach ($matches as $match) {
@@ -350,11 +345,14 @@ class RestServer
 					if ($url && $url[strlen($url) - 1] == '/') {
 						$url = substr($url, 0, -1);
 					}
+
 					$call = array($class, $method->getName());
 					$args = array();
+
 					foreach ($params as $param) {
 						$args[$param->getName()] = $param->getPosition();
 					}
+
 					$call[] = $args;
 					$call[] = null;
 					$call[] = $noAuth;
@@ -365,8 +363,7 @@ class RestServer
 		}
 	}
 
-	public function getPath()
-	{
+	public function getPath() {
 		$path = preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']);
 
 		// remove root from path
@@ -384,27 +381,29 @@ class RestServer
 		return ltrim($path, '/');
 	}
 
-	public function getMethod()
-	{
+	public function getMethod() {
 		$method = $_SERVER['REQUEST_METHOD'];
 		$override = isset($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE']) ? $_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'] : (isset($_GET['method']) ? $_GET['method'] : '');
+
 		if ($method == 'POST' && strtoupper($override) == 'PUT') {
 			$method = 'PUT';
-		} elseif ($method == 'POST' && strtoupper($override) == 'DELETE') {
+		} else if ($method == 'POST' && strtoupper($override) == 'DELETE') {
 			$method = 'DELETE';
-		} elseif ($method == 'POST' && strtoupper($override) == 'PATCH') {
-            $method = 'PATCH';
-        }
+		} else if ($method == 'POST' && strtoupper($override) == 'PATCH') {
+			$method = 'PATCH';
+		}
+
 		return $method;
 	}
 
-	public function getFormat()
-	{
+	public function getFormat() {
 		$format = RestFormat::PLAIN;
 		$accept_mod = null;
-		if(isset($_SERVER["HTTP_ACCEPT"])) {
+
+		if (isset($_SERVER["HTTP_ACCEPT"])) {
 			$accept_mod = preg_replace('/\s+/i', '', $_SERVER['HTTP_ACCEPT']); // ensures that exploding the HTTP_ACCEPT string does not get confused by whitespaces
 		}
+
 		$accept = explode(',', $accept_mod);
 		$override = '';
 
@@ -416,7 +415,7 @@ class RestServer
 		}
 
 		// Check for trailing dot-format syntax like /controller/action.format -> action.json
-		if(preg_match('/\.(\w+)$/i', strtok($_SERVER["REQUEST_URI"],'?'), $matches)) {
+		if (preg_match('/\.(\w+)$/i', strtok($_SERVER["REQUEST_URI"],'?'), $matches)) {
 			$override = $matches[1];
 		}
 
@@ -424,14 +423,14 @@ class RestServer
 		$override = isset($_GET['format']) ? $_GET['format'] : $override;
 		if (isset(RestFormat::$formats[$override])) {
 			$format = RestFormat::$formats[$override];
-		} elseif (in_array(RestFormat::JSON, $accept)) {
+		} else if (in_array(RestFormat::JSON, $accept)) {
 			$format = RestFormat::JSON;
 		}
+
 		return $format;
 	}
 
-	public function getData()
-	{
+	public function getData() {
 		$data = file_get_contents('php://input');
 		$data = json_decode($data, $this->jsonAssoc);
 
@@ -439,8 +438,7 @@ class RestServer
 	}
 
 
-	public function sendData($data)
-	{
+	public function sendData($data) {
 		header("Cache-Control: no-cache, must-revalidate");
 		header("Expires: 0");
 		header('Content-Type: ' . $this->format);
@@ -453,6 +451,7 @@ class RestServer
 					unset($data->$prop);
 				}
 			}
+
 			$this->xml_encode($data);
 		} else {
 			if (is_object($data) && method_exists($data, '__keepOut')) {
@@ -461,6 +460,7 @@ class RestServer
 					unset($data->$prop);
 				}
 			}
+
 			$options = 0;
 			if ($this->mode == 'debug' && defined('JSON_PRETTY_PRINT')) {
 				$options = JSON_PRETTY_PRINT;
@@ -474,8 +474,7 @@ class RestServer
 		}
 	}
 
-	public function setStatus($code)
-	{
+	public function setStatus($code) {
 		if (function_exists('http_response_code')) {
 			http_response_code($code);
 		} else {
@@ -485,42 +484,37 @@ class RestServer
 		}
 	}
 
-	private function xml_encode($mixed, $domElement=null, $DOMDocument=null) {  //@todo add type hint for $domElement and $DOMDocument
+	private function xml_encode($mixed, $domElement = null, $DOMDocument = null) {  //@todo add type hint for $domElement and $DOMDocument
 		if (is_null($DOMDocument)) {
-			$DOMDocument =new DOMDocument;
+			$DOMDocument = new DOMDocument;
 			$DOMDocument->formatOutput = true;
 			$this->xml_encode($mixed, $DOMDocument, $DOMDocument);
 			echo $DOMDocument->saveXML();
-		}
-		else {
-			if (is_array($mixed)) {
-				foreach ($mixed as $index => $mixedElement) {
-					if (is_int($index)) {
-						if ($index === 0) {
-							$node = $domElement;
-						}
-						else {
-							$node = $DOMDocument->createElement($domElement->tagName);
-							$domElement->parentNode->appendChild($node);
-						}
+		} else if (is_array($mixed)) {
+			foreach ($mixed as $index => $mixedElement) {
+				if (is_int($index)) {
+					if ($index === 0) {
+						$node = $domElement;
+					} else {
+						$node = $DOMDocument->createElement($domElement->tagName);
+						$domElement->parentNode->appendChild($node);
 					}
-					else {
-						$plural = $DOMDocument->createElement($index);
-						$domElement->appendChild($plural);
-						$node = $plural;
-						if (!(rtrim($index, 's') === $index)) {
-							$singular = $DOMDocument->createElement(rtrim($index, 's'));
-							$plural->appendChild($singular);
-							$node = $singular;
-						}
-					}
+				} else {
+					$plural = $DOMDocument->createElement($index);
+					$domElement->appendChild($plural);
+					$node = $plural;
 
-					$this->xml_encode($mixedElement, $node, $DOMDocument);
+					if (!(rtrim($index, 's') === $index)) {
+						$singular = $DOMDocument->createElement(rtrim($index, 's'));
+						$plural->appendChild($singular);
+						$node = $singular;
+					}
 				}
+
+				$this->xml_encode($mixedElement, $node, $DOMDocument);
 			}
-			else {
-				$domElement->appendChild($DOMDocument->createTextNode($mixed));
-			}
+		} else {
+			$domElement->appendChild($DOMDocument->createTextNode($mixed));
 		}
 	}
 
